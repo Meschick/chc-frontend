6058d3887e67f35704e27efdee69b33a0897a1a2
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-06-11 11:50:16 -0300

    feat: Configurações necessárias para a lib PrimeNg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: []
 })
 export class AppComponent {
   title = 'chc-frontend';
+
+  constructor(private messageService: MessageService) { }
+  show() {
+    this.messageService.add({ severity: 'success', summary: 'Info', detail: 'Message Content', life: 3000 });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { Toast } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
+import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import { Noir } from '../../style';
+import { ConfirmationService, MessageService } from 'primeng/api';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    Toast,
+    ButtonModule,
+    Ripple
+
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: Noir,
+        options: {
+          darkModeSelector: '.my-app-dark',
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng'
+          }
+        }
+      }
+    }),
+    provideAnimations(),
+    MessageService,
+    ConfirmationService
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }

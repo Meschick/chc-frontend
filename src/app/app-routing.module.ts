e89f0f2f1f1762b98b9c513dfe8e1236a09d17d2
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-06-28 19:49:46 -0300

    feat: Implementa rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './shared/components/header/header.component';
 import { BaseLayoutComponent } from './pages/base-layout/base-layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    component: BaseLayoutComponent
-  }
+    path: '',
+    component: BaseLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+    ]
+  },
 ];
 
 @NgModule({

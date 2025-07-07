ee3768f2ecdc27bc482a8de0d0e071479db5e9dc
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-07-06 23:53:00 -0300

    feat: Cria componente global de card

---
+++ src/app/shared/components/card-dashboard/card-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-dashboard',
+  standalone: false,
+  templateUrl: './card-dashboard.component.html',
+  styleUrl: './card-dashboard.component.scss'
+})
+export class CardDashboardComponent {
+
+}
